23e73fc64571edee1dc167248763398954479519
Andreas Leeb <leeb.andreas00@gmail.com>
2023-05-13 16:17:09 +0200

    Made viewer work

--- components/import/ImportPDF.vue
+++ components/import/ImportPDF.vue
@@ -1,6 +1,6 @@
 <template>
     <input type="file" class="btn btn-primary" @change="onFileChanged" accept=".pdf" />
-    <button @click="clicked">Test</button>
+    <!-- <canvas id="my-canvas"></canvas> -->
     <div id="pageContainer">
         <div id="viewer" class="pdfViewer"></div>
     </div>
@@ -10,8 +10,12 @@
 #pageContainer {
     position: absolute;
 }
-</style>
 
+/* #my-canvas {
+    border: 1px solid black;
+    direction: ltr;
+} */
+</style>
 <script setup lang="ts">
 import { PDFDocument } from 'pdf-lib';
 import pdfjs from "@bundled-es-modules/pdfjs-dist/build/pdf";
@@ -19,16 +23,12 @@ import viewer from "@bundled-es-modules/pdfjs-dist/web/pdf_viewer";
 
 pdfjs.GlobalWorkerOptions.workerSrc = "_nuxt/node_modules/@bundled-es-modules/pdfjs-dist/build/pdf.worker.js";
 
-function clicked() {
-    console.log("click");
-}
-
-const file = ref<File | null>();
-const form = ref<HTMLFormElement>();
+let cont: HTMLDivElement;
+let pdfViewer: viewer.PDFViewer;
 
 function readFileAsync(file: File): Promise<ArrayBuffer | null> {
     return new Promise((resolve, reject) => {
-        let reader = new FileReader();
+        const reader = new FileReader();
         reader.onload = () => {
             resolve(reader.result as (ArrayBuffer | null));
         };
@@ -38,44 +38,57 @@ function readFileAsync(file: File): Promise<ArrayBuffer | null> {
 }
 
 async function onFileChanged($event: Event): Promise<void> {
+    cont ??= document.getElementById("pageContainer") as HTMLDivElement;
+    pdfViewer ??= new viewer.PDFViewer({
+        container: cont as HTMLDivElement,
+        eventBus: new viewer.EventBus(),
+        linkService: new viewer.PDFLinkService(),
+        l10n: {
+            async getLanguage(): Promise<string> {
+                return "en-US";
+            },
+            async getDirection(): Promise<string> {
+                return "";
+            },
+            async get(key: any, args?: null, fallback?: any): Promise<any> {
+                return null;
+            },
+            async translate(element: any): Promise<void> {
+
+            }
+        },
+        textLayerMode: 0
+    });
+
     console.log("file changed");
     const target = $event.target as HTMLInputElement;
     if (target && target.files) {
         console.log("file valid");
-        file.value = target.files[0]
 
-        const bytes = await readFileAsync(file.value);
+        const bytes = await readFileAsync(target.files[0]);
         if (bytes) {
             console.log("file read");
             const pdfDoc = await PDFDocument.load(bytes);
             console.log("pages: " + pdfDoc.getPageCount());
             //const firstPage = pdfDoc.getPage(0);
             const newDoc = await PDFDocument.create();
-            await newDoc.copyPages(pdfDoc, [0]);
-            //newDoc.addPage(test[0]);
+            // const newDoc = await pdfDoc.copy();
+            const copiedPages = await newDoc.copyPages(pdfDoc, [1]);
+            copiedPages.forEach((page, idx, _) => newDoc.addPage(page));
             const byteArray = await newDoc.save();
-            const doc = await pdfjs.getDocument(byteArray).promise;
-            let cont = document.getElementById("pageContainer") as HTMLDivElement;
-            let pdfViewer = new viewer.PDFViewer({
-                container: cont as HTMLDivElement,
-                eventBus: new viewer.EventBus(),
-                linkService: new viewer.PDFLinkService(),
-                l10n: {
-                    async getLanguage(): Promise<string> {
-                        return "en-US";
-                    },
-                    async getDirection(): Promise<string> {
-                        return "";
-                    },
-                    async get(key: any, args?: null, fallback?: any): Promise<any> {
-                        return null;
-                    },
-                    async translate(element: any): Promise<void> {
 
-                    }
-                }
-            });
+            const doc = await pdfjs.getDocument(byteArray).promise;
             pdfViewer.setDocument(doc);
+
+            // custom rendering
+            // const page1 = await doc.getPage(1);
+            // const viewport = page1.getViewport({ scale: 1 });
+            // const canvas = document.getElementById("my-canvas") as HTMLCanvasElement;
+            // const context = canvas.getContext("2d") as CanvasRenderingContext2D;
+            // canvas.height = viewport.height;
+            // canvas.width = viewport.width;
+            // console.log(`height: ${canvas.height}, width: ${canvas.width}`);
+            // await page1.render({ canvasContext: context, viewport: viewport }).promise;
         }
     }
 }
