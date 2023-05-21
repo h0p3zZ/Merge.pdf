c3d55beef7f0dae967352bc35e6a56b6fbef8af9
Andreas Leeb <leeb.andreas00@gmail.com>
2023-05-21 20:58:05 +0200

    Fix error

--- app.vue
+++ app.vue
@@ -3,21 +3,15 @@
       <ImportPDF @change="importChanged"></ImportPDF>
       <ExportPDF :pdfData="pdfData"></ExportPDF>
     <ClientOnly placeholder="loading...">
-      <DisplayPDF :pdfData="pdfData" :pdf-doc="pdf"></DisplayPDF>
+      <DisplayPDF :pdfData="pdfData"></DisplayPDF>
     </ClientOnly>
   </div>
 </template>
 
 <script setup lang="ts">
-import { PDFDocument } from 'pdf-lib';
-import pdfjs, { PDFPageProxy, PDFDocumentProxy } from "@bundled-es-modules/pdfjs-dist/build/pdf";
-
-const pdf = ref<PDFDocumentProxy>();
 const pdfData = ref<ArrayBuffer>(new ArrayBuffer(0));
 
-async function importChanged(pdfD: ArrayBuffer) {
+function importChanged(pdfD: ArrayBuffer) {
   pdfData.value = pdfD;
-  pdf.value = await pdfjs.getDocument(pdfD).promise;
 }
-
 </script>
