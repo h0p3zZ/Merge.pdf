310f750db537a603b604a35f6f20f7306e90dac5
h0p3zZ <h0p3zZ@outlook.com>
2023-06-03 23:27:38 +0200

    added bootstrap js

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,4 +7,7 @@ export default defineNuxtConfig({
     css: [
         "~/node_modules/bootstrap/dist/css/bootstrap.min.css"
     ],
+    plugins: [
+        "~/plugins/bootstrap.client.ts"
+    ],
 })
