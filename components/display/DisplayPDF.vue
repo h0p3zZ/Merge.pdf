5a362a6c5355a75dccaf8b76abf34496e30ccafb
h0p3zZ <h0p3zZ@outlook.com>
2023-06-09 16:31:35 +0200

    changed typo in doc of DisplayPDF.vue

--- components/display/DisplayPDF.vue
+++ components/display/DisplayPDF.vue
@@ -11,7 +11,7 @@
                 <path
                     d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
             </svg>
-            <!-- Droppable is where the PDF page can be dropped to -->
+            <!-- Dropable is where the PDF page can be dropped to -->
             <div class="dropable" @drop="drop($event, +i)" @dragover.prevent @dragenter="dragenter" @dragleave="dragleave">
             </div>
         </div>
